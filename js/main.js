a7096d46fd33fffec118c20159797229622392f1
Vaharsolta <vaha-22@mail.ru>
2020-01-02 23:15:11 +0300

    Added function get plane for box

--- js/main.js
+++ js/main.js
@@ -2,8 +2,13 @@ function init() {
     const scene = new THREE.Scene();
     
     const box = getBox(1, 1, 1);
+    const plane = getPlane(4);
+
+    box.position.y = box.geometry.parameters.height/2;
+    plane.rotation.x = Math.PI/2;
 
     scene.add(box);
+    scene.add(plane);
 
     const camera = new THREE.PerspectiveCamera(
         45,
@@ -32,6 +37,19 @@ function getBox(w, h, d) {
     let geometry = new THREE.BoxGeometry(w, h, d);
     let material = new THREE.MeshBasicMaterial({
         color: 0x00ff00
+    });
+
+    return new THREE.Mesh(
+        geometry,
+        material
+    );
+}
+
+function getPlane(size) {
+    let geometry = new THREE.PlaneGeometry(size, size);
+    let material = new THREE.MeshBasicMaterial({
+        color: 0x0000ff,
+        side: THREE.DoubleSide
     });
 
     return new THREE.Mesh(
